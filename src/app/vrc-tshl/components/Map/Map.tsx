ab9d0407968b5e4f75df98ea34b1efc4be9dc5e4
chuyechuan <2911349773@qq.com>
2025-03-31 17:19:54 +0800

    优化地图显示效果

--- src/app/vrc-tshl/components/Map/Map.tsx
+++ src/app/vrc-tshl/components/Map/Map.tsx
@@ -2,27 +2,6 @@ import { LayerPopup, LineLayer, Marker, MarkerLayer, PointLayer, Scene } from '@
 import { GaodeMap } from '@antv/l7-maps';
 import Data from './data';
 
-const scene = new Scene({
-  id: 'map',
-  map: new GaodeMap({
-    token: '95ad3b434f34ec113e9dce368309117e',
-    center: [112.875624, 28.235057],
-    // pitch: 35.210526315789465,
-    style: 'dark',
-    pitchEnable: false,
-    // rotation: -90,
-    mapStyle: 'amap://styles/darkblue',
-    zoom: 15,
-  }),
-});
-
-scene.on('loaded', () => {
-
-  Data.line.forEach((item) => {
-    scene.addLayer(addLayer(item.data, item.color));
-  });
-});
-
 const addLayer = (coordinates: number[][], color: string) => {
   const layer = new LineLayer({})
     .source({
@@ -67,10 +46,34 @@ const addLayer = (coordinates: number[][], color: string) => {
 // after:h-full 
 // after:shadow-innerBlack
 
-export default () => (
+export default () => {
+  
+const scene = new Scene({
+  id: 'map',
+  map: new GaodeMap({
+    token: '95ad3b434f34ec113e9dce368309117e',
+    center: [112.875624, 28.235057],
+    // pitch: 35.210526315789465,
+    style: 'dark',
+    pitchEnable: false,
+    // rotation: -90,
+    mapStyle: 'amap://styles/darkblue',
+    zoom: 15,
+  }),
+});
+
+scene.on('loaded', () => {
+  console.log('map loaded');
+
+  Data.line.forEach((item) => {
+    scene.addLayer(addLayer(item.data, item.color));
+  });
+});
+
+  return (
   <div className="w-full 
   h-full 
   justify-center 
   relative 
   " id="map" />
-);
+)};
